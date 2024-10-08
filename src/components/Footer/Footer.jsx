a2f7c916a9ac3ef8d8f2fbd328dbcb360c3d83b7
ShamsiddinWeb <shamsiddinnorov734@gmail.com>
2024-10-08 20:42:33 +0500

    finish

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -20,16 +20,20 @@ const Footer = (props) => {
     <footer className="footer">
       <div className="container">
         <div className="footer__start">
-          <Link to={"/"} data-aos="fade-right">
+          <a href="#" data-aos="fade-right">
             <img className="footer__img" src={footerIcon} alt="" width={250} />
-          </Link>
+          </a>
           <div className="footer__card">
             <Link className="footer__link" to={"/"} data-aos="fade-right">
               <CiLocationOn className="footer__icon" />
               <p className="footer__text">{props.t("footer-title")}</p>
               <GoArrowUpRight className="footer__icon2" />
             </Link>
-            <Link className="footer__link" to={"Tel:998940337212"} data-aos="fade-right">
+            <Link
+              className="footer__link"
+              to={"Tel:998940337212"}
+              data-aos="fade-right"
+            >
               <BsTelephone className="footer__icon" />
               <p className="footer__text">+998 94 033 72 12</p>
               <GoArrowUpRight className="footer__icon2" />
